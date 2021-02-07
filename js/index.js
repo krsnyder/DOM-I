41da3abd08bb247119b012948ba6ff6a158d5bc2
Kirk <kirkrobertsnyder@gmail.com>
2021-02-07 14:36:20 -0500

    Updated text content in nav links

--- js/index.js
+++ js/index.js
@@ -40,3 +40,12 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelectorAll('a');
+nav.forEach(element => {
+  element.textContent = siteContent["nav"][element]
+});
+  
+
+
+
